fb51fec50356d951921abe4276859ee6d59afa9f
maxklema <maxklema@gmail.com>
2025-08-18 22:38:03 +0000

    bug fixes to gather runtime script

--- container creation/deployment-scripts/gatherRuntimeLangs.sh
+++ container creation/deployment-scripts/gatherRuntimeLangs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Keep track of the runtime languages and versions for each component
+VERSIONS_DICT={}
+
 gatherRunTime() {
     COMPONENT_PATH="$1"
 
@@ -7,7 +10,9 @@ gatherRunTime() {
         read -p "🖥️  Enter the underlying runtime environment for \"$COMPONENT_PATH\" (e.g., 'nodejs', 'python') →  " RUNTIME_LANGUAGE
     fi
 
-    while [ "${RUNTIME_LANGUAGE^^}" != "NODEJS" ] && [ "${RUNTIME_LANGUAGE^^}" != "PYTHON" ]; do
+    RUNTIME_LANGUAGE_REGEX="^(python|nodejs)(@([0-9]+(\.[0-9]+){0,2}))?$"
+
+    while [[ ! ${RUNTIME_LANGUAGE,,} =~ $RUNTIME_LANGUAGE_REGEX ]]; do
         echo "⚠️  Sorry, that runtime environment is not yet supported. Only \"nodejs\" and \"python\" are currently supported."
         writeLog "Unsupported runtime environment entered: $RUNTIME_LANGUAGE for component: $COMPONENT_PATH"
         if [ "${GH_ACTION^^}" == "Y" ]; then
@@ -17,6 +22,22 @@ gatherRunTime() {
         fi
         read -p "🖥️  Enter the underlying runtime environment for \"$COMPONENT_PATH\" (e.g., 'nodejs', 'python') →  " RUNTIME_LANGUAGE
     done
+
+    set -x
+
+    # Scrape runtime version
+    if [[ "${RUNTIME_LANGUAGE,,}" == *"@"* ]]; then
+        if [ "${MULTI_COMPONENT^^}" == "Y" ]; then
+            RUNTIME_VERSION=$(echo "${RUNTIME_LANGUAGE,,}" | sed -E 's/.*@([0-9]+(\.[0-9]+){0,2}).*/\1/')
+            UPDATED_VERSIONS_DICT=$(echo "$VERSIONS_DICT" | jq --arg k "$COMPONENT_PATH" --arg v "$RUNTIME_VERSION" '. + {($k): $v}')
+            VERSIONS_DICT=$UPDATED_VERSIONS_DICT
+        else
+            RUNTIME_VERSION=$(echo "${RUNTIME_LANGUAGE,,}" | sed -E 's/.*@([0-9]+(\.[0-9]+){0,2}).*/\1/')
+            UPDATED_VERSIONS_DICT=$(echo "$VERSIONS_DICT" | jq --arg k "default" --arg v "$RUNTIME_VERSION" '. + {($k): $v}')
+            VERSIONS_DICT=$UPDATED_VERSIONS_DICT
+        fi
+    fi
+    RUNTIME_LANGUAGE=$(echo "${RUNTIME_LANGUAGE,,}" | sed -E 's/@.*//')
 }
 
 # Helper function to remove an item from a list
@@ -39,7 +60,31 @@ if [ "${MULTI_COMPONENT^^}" == 'Y' ]; then
     if [ ! -z "$RUNTIME_LANGUAGE" ]; then # Environment Variable Passed
         if echo "$RUNTIME_LANGUAGE" | jq -e > /dev/null 2>&1; then # Valid JSON
             for key in $(echo "$RUNTIME_LANGUAGE" | jq -r 'keys[]'); do
+                RUNTIME_LANGUAGE_REGEX="^(python|nodejs)(@([0-9]+(\.[0-9]+){0,2}))?$"
+                COMPONENT_RUNTIME_LANGUAGE=$(echo "$RUNTIME_LANGUAGE" | jq -r --arg key "$key" '.[$key]')
+                while [[ ! ${COMPONENT_RUNTIME_LANGUAGE,,} =~ $RUNTIME_LANGUAGE_REGEX ]]; do
+                    if [ "${GH_ACTION^^}" == "Y" ]; then
+                        outputError "⚠️  Sorry, that runtime environment is not yet supported. Only \"nodejs\" and \"python\" are currently supported."
+                        writeLog "Unsupported runtime environment entered: $COMPONENT_RUNTIME_LANGUAGE (GH_ACTION mode)"
+                        exit 17
+                    fi
+                    echo "⚠️  Sorry, that runtime environment is not yet supported. Only \"nodejs\" and \"python\" are currently supported."
+                    writeLog "Unsupported runtime environment entered: $COMPONENT_RUNTIME_LANGUAGE for component: $COMPONENT_PATH"
+                    read -p "🖥️  Enter the underlying runtime environment for \"$COMPONENT_PATH\" (e.g., 'nodejs', 'python') →  " COMPONENT_RUNTIME_LANGUAGE
+                done
+
+                # Scrape runtime version
+                if [[ "${COMPONENT_RUNTIME_LANGUAGE,,}" == *"@"* ]]; then
+                    RUNTIME_VERSION=$(echo "${RUNTIME_LANGUAGE,,}" | sed -E 's/.*@([0-9]+(\.[0-9]+){0,2}).*/\1/')
+                    UPDATED_VERSIONS_DICT=$(echo "$VERSIONS_DICT" | jq --arg k "$key" --arg v "$RUNTIME_VERSION" '. + {($k): $v}')
+                    VERSIONS_DICT=$UPDATED_VERSIONS_DICT
+                fi
+                COMPONENT_RUNTIME_LANGUAGE=$(echo "${COMPONENT_RUNTIME_LANGUAGE,,}" | sed -E 's/@.*//')
+                RUNTIME_LANGUAGE=$(echo "$RUNTIME_LANGUAGE" \
+                  | jq -c --arg k "$key" --arg v "$COMPONENT_RUNTIME_LANGUAGE" '.[$k] = $v')
+
                 removeFromList "$key"
+
             done
             if [ ${#UNIQUE_COMPONENTS_CLONE[@]} -gt 0 ]; then #if there are still components in the list, then not all runtimes were provided, so exit on error
                 if [ "${GH_ACTION^^}" == "Y" ]; then
